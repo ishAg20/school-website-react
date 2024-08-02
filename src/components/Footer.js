db964bdccf40c5e5afcfa3ed84c4569a284e9d8b
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-03 00:44:29 +0530

    Update Footer.js : Add ARIA roles for accessibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer fadeIn">
+    <footer className="footer fadeIn" role="contentinfo">
       <div className="container mx-auto">
         <p>&copy; 2024 The Heritage School. All rights reserved.</p>
       </div>
