3bd5062756e09cbd2f062840d8641a9abf5f3d6d
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-03 00:54:44 +0530

    Update Navbar.js : Add Keyboard Navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,38 +7,23 @@ const Navbar = () => {
     <nav className="navbar" role="navigation" aria-label="Main Navigation">
       <div className="logo-container">
         <img src="/logo.png" alt="School Logo" />
-        <Link className="nav-link-main nav-link" to="/" role="link">
+        <Link className="nav-link-main nav-link" to="/" role="link" tabIndex="0">
           The Heritage School
         </Link>
       </div>
       <div className="nav-links">
-        <Link className="nav-link" to="/" role="link">
-          Home
-        </Link>
-        <Link className="nav-link" to="/about-us" role="link">
-          About Us
-        </Link>
-        <Link className="nav-link" to="/academics" role="link">
-          Academics
-        </Link>
-        <Link className="nav-link" to="/admissions" role="link">
-          Admissions
-        </Link>
-        <Link className="nav-link" to="/faculty" role="link">
-          Faculty
-        </Link>
-        <Link className="nav-link" to="/students" role="link">
-          Students
-        </Link>
-        <Link className="nav-link" to="/gallery" role="link">
-          Gallery
-        </Link>
-        <Link className="nav-link" to="/contact-us" role="link">
-          Contact Us
-        </Link>
+        <Link className="nav-link" to="/" role="link" tabIndex="0">Home</Link>
+        <Link className="nav-link" to="/about-us" role="link" tabIndex="0">About Us</Link>
+        <Link className="nav-link" to="/academics" role="link" tabIndex="0">Academics</Link>
+        <Link className="nav-link" to="/admissions" role="link" tabIndex="0">Admissions</Link>
+        <Link className="nav-link" to="/faculty" role="link" tabIndex="0">Faculty</Link>
+        <Link className="nav-link" to="/students" role="link" tabIndex="0">Students</Link>
+        <Link className="nav-link" to="/gallery" role="link" tabIndex="0">Gallery</Link>
+        <Link className="nav-link" to="/contact-us" role="link" tabIndex="0">Contact Us</Link>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
