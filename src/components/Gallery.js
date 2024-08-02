7098152c0353591aad4dee3d5543f298c80d7d66
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-03 00:10:07 +0530

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./Gallery.css";
 
 const images = [
-  "image1.jpg",
+  "image1.jpeg",
   "image2.jpg",
   "image3.jpg",
-  "image4.jpg",
+  "image4.jpeg",
   "image5.webp",
   "image6.jpg",
   "image7.jpg",
-  "image8.jpg",
+  "image8.jpeg",
   "image9.jpg",
-  "image10.jpg",
+  "image10.jpeg",
 ];
 
 const Gallery = () => {
