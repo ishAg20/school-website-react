8abee3aa3db583495b3d7192af88b0010b90631f
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-03 00:50:03 +0530

    Update Navbar.js : Add ARIA roles for accessibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,36 +4,36 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar" role="navigation" aria-label="Main Navigation">
       <div className="logo-container">
         <img src="/logo.png" alt="School Logo" />
-        <Link className="nav-link-main nav-link" to="/">
+        <Link className="nav-link-main nav-link" to="/" role="link">
           The Heritage School
         </Link>
       </div>
       <div className="nav-links">
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="/" role="link">
           Home
         </Link>
-        <Link className="nav-link" to="/about-us">
+        <Link className="nav-link" to="/about-us" role="link">
           About Us
         </Link>
-        <Link className="nav-link" to="/academics">
+        <Link className="nav-link" to="/academics" role="link">
           Academics
         </Link>
-        <Link className="nav-link" to="/admissions">
+        <Link className="nav-link" to="/admissions" role="link">
           Admissions
         </Link>
-        <Link className="nav-link" to="/faculty">
+        <Link className="nav-link" to="/faculty" role="link">
           Faculty
         </Link>
-        <Link className="nav-link" to="/students">
+        <Link className="nav-link" to="/students" role="link">
           Students
         </Link>
-        <Link className="nav-link" to="/gallery">
+        <Link className="nav-link" to="/gallery" role="link">
           Gallery
         </Link>
-        <Link className="nav-link" to="/contact-us">
+        <Link className="nav-link" to="/contact-us" role="link">
           Contact Us
         </Link>
       </div>
