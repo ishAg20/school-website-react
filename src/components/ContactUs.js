82443519b8d05e1166f57bcff4dd16a326b5ee56
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-08-03 00:48:30 +0530

    Update ContactUs.js : Modify styles

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,8 +4,11 @@ import "./ContactUs.css";
 
 const ContactUs = () => {
   return (
-    <div className="contact-us fadeIn"  role="region"
-      aria-labelledby="contact-us-title">
+    <div
+      className="contact-us fadeIn"
+      role="region"
+      aria-labelledby="contact-us-title"
+    >
       <h1 id="contact-us-title">Contact Us</h1>
       <p>
         For any queries or information regarding admissions, events, or general
@@ -18,8 +21,7 @@ const ContactUs = () => {
       <h1>Visit Us</h1>
       <p>Address: 1234 Heritage Road, Rohini, New Delhi, India</p>
       <div className="map-container" role="region" aria-labelledby="map-title">
-      <h2 id="map-title">Location Map</h2>    
-     <iframe
+        <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.648059389099!2d77.0861084151131!3d28.692662082406865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1c2381a3e30b%3A0x37a660645317d14a!2sThe%20Heritage%20School%2C%20Rohini!5e0!3m2!1sen!2sin!4v1632427929538!5m2!1sen!2sin"
           width="600"
           height="450"
